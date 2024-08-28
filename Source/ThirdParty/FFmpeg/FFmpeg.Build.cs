777eafed6c1273aa11a72c57543cd7f202434d5c
Udon-Tobira <146440502+Udon-Tobira@users.noreply.github.com>
2024-08-29 05:37:57 +0900

    add ffmpeg build files

---
+++ Source/ThirdParty/FFmpeg/FFmpeg.Build.cs
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using System.IO;
+using UnrealBuildTool;
+
+public class FFmpeg : ModuleRules
+{
+    public FFmpeg(ReadOnlyTargetRules Target) : base(Target)
+    {
+        Type = ModuleType.External;
+
+        // get FFmpeg directory path
+        var FFmpegDirectoryPath = Path.Combine(ModuleDirectory, "FFmpeg");
+
+        // get FFmpeg libavcodec directory path
+        var FFmpegIncludeDirectoryPath = Path.Combine(FFmpegDirectoryPath, "bin", "include");
+
+        // get FFmpeg lib directory path
+        var FFmpegLibDirectoryPath = Path.Combine(FFmpegDirectoryPath, "bin");
+
+        // get FFmpeg bin directory path
+        var FFmpegBinDirectoryPath = Path.Combine(FFmpegDirectoryPath, "bin");
+
+        PublicSystemIncludePaths.Add(FFmpegIncludeDirectoryPath);
+
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            var FFmpegDllFilePath = Path.Combine(FFmpegBinDirectoryPath, "*.dll");
+
+            // Add the import library
+            PublicAdditionalLibraries.Add(Path.Combine(FFmpegLibDirectoryPath, "avcodec.lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(FFmpegLibDirectoryPath, "avformat.lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(FFmpegLibDirectoryPath, "swscale.lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(FFmpegLibDirectoryPath, "avutil.lib"));
+            // Delay-load the DLL, so we can load it from the right place first
+            PublicDelayLoadDLLs.Add(Path.Combine(FFmpegDllFilePath));
+
+
+            // Ensure that the DLLs are staged along with the executable
+            RuntimeDependencies.Add("$(BinaryOutputDir)", FFmpegDllFilePath);
+        }
+        else if (Target.Platform == UnrealTargetPlatform.Mac)
+        {
+            var FFmpegDylibFilePath = Path.Combine(FFmpegBinDirectoryPath, "Release", "*.dylib");
+
+            // Delay-load the DLL, so we can load it from the right place first
+            PublicDelayLoadDLLs.Add(Path.Combine(FFmpegDylibFilePath));
+
+            // Ensure that the DLL is staged along with the executable
+            RuntimeDependencies.Add("$(BinaryOutputDir)", FFmpegDylibFilePath);
+        }
+        else if (Target.Platform == UnrealTargetPlatform.Android)
+        {
+            // Add the import library
+            PublicAdditionalLibraries.Add(Path.Combine(FFmpegBinDirectoryPath, "arm64-v8a", "*.so"));
+        }
+        else if (Target.Platform == UnrealTargetPlatform.Linux)
+        {
+            string LibFFmpegSoPath = Path.Combine(FFmpegBinDirectoryPath, "*.so");
+
+            PublicAdditionalLibraries.Add(LibFFmpegSoPath);
+            PublicDelayLoadDLLs.Add(LibFFmpegSoPath);
+            RuntimeDependencies.Add(LibFFmpegSoPath);
+        }
+    }
+}
